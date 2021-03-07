633ae69223a2b6d736e32390ef7e103fe148559a
Ilias Chatziefstathiou <liakou.metal@gmail.com>
2021-03-08 01:15:56 +0200

    Sky done, HomepageMusic start
    Created the play/pause images and added the .mp3 for the music on the home page
    Also finished the sky/lighting changing scripts

--- JavaScript/functions.js
+++ JavaScript/functions.js
@@ -1,28 +1,54 @@
-
 //Changes the page colors depending on the current time
-
 function backgroundDayColor() {
     var h = new Date;
-    if (h.getHours() < 6){
+    if (h.getHours() < 6) {
         document.body.style.backgroundColor = "#e0cba4";
         document.getElementById("sky").style.backgroundColor = "#464646";
     }
-    else if (h.getHours() < 7){
+    else if (h.getHours() < 7) {
         document.body.style.backgroundColor = "#f7d7ab";
         document.getElementById("sky").style.backgroundColor = "#ffb788";
     }
-    else if (h.getHours() < 19){
+    else if (h.getHours() < 19) {
         document.body.style.backgroundColor = "#efe1c5";
         document.getElementById("sky").style.backgroundColor = "#d5ecff";
     }
-    else if (h.getHours() < 20){
+    else if (h.getHours() < 20) {
         document.body.style.backgroundColor = "#f7d7ab";
         document.getElementById("sky").style.backgroundColor = "#ffb788";
     }
-    else
-    {
+    else {
         document.body.style.backgroundColor = "#e0cba4";
         document.getElementById("sky").style.backgroundColor = "#464646";
     }
+
+    //This segment code helps debug by changing background every minute in stead of hour
+    //
+    // if (h.getMinutes()%3 === 0) {
+    //     document.body.style.backgroundColor = "#e0cba4";
+    //     document.getElementById("sky").style.backgroundColor = "#464646";
+    // }
+    // else if (h.getMinutes()%3 === 1) {
+    //     document.body.style.backgroundColor = "#f7d7ab";
+    //     document.getElementById("sky").style.backgroundColor = "#ffb788";
+    // }
+    // else if (h.getMinutes()%3 === 2) {
+    //     document.body.style.backgroundColor = "#efe1c5";
+    //     document.getElementById("sky").style.backgroundColor = "#d5ecff";
+    // }
 }
 
+// Sets transition for backgrounds to 10s
+function transitionSet() {
+    document.body.style.transitionDuration = "10s";
+    document.getElementById("sky").style.transitionDuration = "10s";
+}
+
+//Play audio
+
+function playAudio(x) {
+    x.play();
+}
+function pauseAudio(x) {
+    x.pause();
+}
