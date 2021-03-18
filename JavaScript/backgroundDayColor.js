cb083aaed188491f5d318d5c1bc70af237c7ced4
Ilias Chatziefstathiou <liakou.metal@gmail.com>
2021-03-18 23:37:42 +0200

    Projects appearance
    Finalized the buttons for the sorting in projects page
    Also made different .js files for all the different js so far
    Time to write the sorting scripts

--- JavaScript/backgroundDayColor.js
+++ JavaScript/backgroundDayColor.js
@@ -1,5 +1,5 @@
 //Changes the page colors depending on the current time
-function backgroundDayColor() {
+function backgroundDayColor(){
     var h = new Date;
     if (h.getHours() < 6) {
         document.body.style.backgroundColor = "#e0cba4";
@@ -21,4 +21,32 @@ function backgroundDayColor() {
         document.body.style.backgroundColor = "#e0cba4";
         document.getElementById("sky").style.backgroundColor = "#464646";
     }
+
+    //This segment code helps debug by changing background every minute in stead of hour
+    //
+    // if (h.getMinutes()%3 === 0) {
+    //     document.body.style.backgroundColor = "#e0cba4";
+    //     document.getElementById("sky").style.backgroundColor = "#464646";
+    // }
+    // else if (h.getMinutes()%3 === 1) {
+    //     document.body.style.backgroundColor = "#f7d7ab";
+    //     document.getElementById("sky").style.backgroundColor = "#ffb788";
+    // }
+    // else if (h.getMinutes()%3 === 2) {
+    //     document.body.style.backgroundColor = "#efe1c5";
+    //     document.getElementById("sky").style.backgroundColor = "#d5ecff";
+    // }
 }
+
+//Sets transition for backgrounds to 10s
+function transitionSet() {
+    document.body.style.transitionDuration = "10s";
+    document.getElementById("sky").style.transitionDuration = "10s";
+}
+
+//Auto executes daylight stuff
+document.addEventListener("DOMContentLoaded", function(){
+    backgroundDayColor();					//Initial page coloring based on time of day
+    setTimeout(transitionSet, 30);			//Adding transition is delayed by 30ms so that the first page coloring happens instantly
+    setInterval(backgroundDayColor, 60000);	//Every 60s this script looks if there should be a change in sky coloring
+});
---
+++ JavaScript/homepageAudio.js
@@ -0,0 +1,38 @@
+
+
+
+//Play and pause audio
+var paused = true;
+function doAudio(AudioID) {
+    var myAudio = document.getElementById(AudioID);
+    if (!paused){
+        document.getElementById("PlayPauseDesktop").style.backgroundPosition ="0px 0";
+        document.getElementById("PlayPauseMobile").style.backgroundPosition ="0px 0";
+        paused = true;
+        $("audio").stop();
+        $("audio").animate({volume:0}, 1000);
+		var int_pause=setInterval(function(){
+                if (document.getElementById("myAudio").volume == 0){
+                    myAudio.pause();
+                    clearInterval(int_pause);
+                } 
+            }, 100); //When audio volume falls to 0 pause audio and stop looking to pause
+
+    }
+    else{
+        document.getElementById("PlayPauseDesktop").style.backgroundPosition ="-17px 0";
+        document.getElementById("PlayPauseMobile").style.backgroundPosition ="-17px 0";
+        paused = false;
+        $("audio").stop();
+        myAudio.play();
+        $("audio").animate({volume:1}, 1000);
+    }
+}
+
+function showPlay() //Shows the play button again when audio ends
+{
+    document.getElementById("PlayPauseDesktop").style.backgroundPosition ="0px 0";
+    document.getElementById("PlayPauseMobile").style.backgroundPosition ="0px 0";
+    document.getElementById("myAudio").volume = 0;
+    paused = true;
+}
