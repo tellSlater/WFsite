7da313341876b279885a2bcd8a1c80498907620b
Ilias Chatziefstathiou <liakou.metal@gmail.com>
2021-12-02 15:51:09 +0200

    Projects Dash Line
    Changed toggle to opacity 0-1 hpoing that the img will be preloaded now and wont take forever to appear (bad UI)

--- JavaScript/projects.js
+++ JavaScript/projects.js
@@ -1,53 +1,50 @@
 //All scripts needed in the projects page
 var
-by = "",            //Property that the list of projects is sorted by
-easy = true,        //Visibility of easy projects
-normal = true,      //Visibility of normal projects
-hard = true;        //Visibility of hard projects
+    by = "",            //Property that the list of projects is sorted by
+    easy = true,        //Visibility of easy projects
+    normal = true,      //Visibility of normal projects
+    hard = true;        //Visibility of hard projects
 
 //Does the sorting
-function sortProjects(btn){
+function sortProjects(btn) {
     var
-    divs = document.getElementById("main").childNodes,
-    a = [],
-    i = 0;
+        divs = document.getElementById("main").childNodes,
+        a = [],
+        i = 0;
 
-    for (i = 0; i < divs.length; i++)
-    {
+    for (i = 0; i < divs.length; i++) {
         if (divs[i].tagName == 'DIV')
-        a.push(divs[i].childNodes[1])
+            a.push(divs[i].childNodes[1])
     }
 
     if ((by != "RANDOM") && (by === btn.innerHTML)) return;
     else by = btn.innerHTML;
 
     $(".cursor").css("display", "none");
-    btn.parentNode.childNodes[3].style.display="block";
-    btn.parentNode.childNodes[5].style.display="block";
+    btn.parentNode.childNodes[3].style.display = "block";
+    btn.parentNode.childNodes[5].style.display = "block";
 
     $(".sortBtn .by").css("cursor", "pointer");
-    if (btn.parentNode.childNodes[1].innerHTML != "RANDOM")
-    {
-        btn.parentNode.childNodes[1].style.cursor="context-menu";
-        btn.parentNode.childNodes[3].style.cursor="context-menu";
-        btn.parentNode.childNodes[5].style.cursor="context-menu";
+    if (btn.parentNode.childNodes[1].innerHTML != "RANDOM") {
+        btn.parentNode.childNodes[1].style.cursor = "context-menu";
+        btn.parentNode.childNodes[3].style.cursor = "context-menu";
+        btn.parentNode.childNodes[5].style.cursor = "context-menu";
     }
 
-    switch (by)
-    {
+    switch (by) {
         case "DATE":
-            a.sort(function(a,b) {
+            a.sort(function (a, b) {
                 var
-                da = Date.parse( a.childNodes[5].innerHTML ),
-                db = Date.parse( b.childNodes[5].innerHTML );
+                    da = Date.parse(a.childNodes[5].innerHTML),
+                    db = Date.parse(b.childNodes[5].innerHTML);
                 return da == db ? 0 : (da > db ? -1 : 1);
             });
             break;
         case "NAME":
-            a.sort(function(a,b) {
+            a.sort(function (a, b) {
                 var
-                na = a.childNodes[3].innerHTML,
-                nb = b.childNodes[3].innerHTML;
+                    na = a.childNodes[3].innerHTML,
+                    nb = b.childNodes[3].innerHTML;
                 return na == nb ? 0 : (na < nb ? -1 : 1);
             });
             break;
@@ -58,7 +55,7 @@ function sortProjects(btn){
             console.log("ERROR - No match for switch in sortProjects function")
     }
 
-    document.getElementById("main").innerHTML= "";
+    document.getElementById("main").innerHTML = "";
 
     for (i = 0; i < a.length; i++) {
         document.getElementById("main").appendChild(a[i].parentNode);
@@ -66,22 +63,20 @@ function sortProjects(btn){
 }
 
 //Toggles easy/normal/hard projects visibility
-function toggleDiff(btn){
+function toggleDiff(btn) {
 
     var
-    dashed = false,
-    divs = document.getElementById("main").childNodes,
-    a = [],
-    i = 0;
+        dashed = false,
+        divs = document.getElementById("main").childNodes,
+        a = [],
+        i = 0;
 
-    for (i = 0; i < divs.length; i++)
-    {
+    for (i = 0; i < divs.length; i++) {
         if (divs[i].tagName == 'DIV')
-        a.push(divs[i].childNodes[1])
+            a.push(divs[i].childNodes[1])
     }
 
-    switch (btn.parentNode.childNodes[1].innerHTML)
-    {
+    switch (btn.parentNode.childNodes[1].innerHTML) {
         case "EASY":
             easy = !easy;
             dashed = !easy;
@@ -101,20 +96,17 @@ function toggleDiff(btn){
             console.log("ERROR - No match for switch in toggleDiff function")
     }
 
-    if (dashed) btn.parentNode.childNodes[3].style.display="block";
-    else btn.parentNode.childNodes[3].style.display="none";
+    if (dashed) btn.parentNode.childNodes[3].style.opacity = "1";
+    else btn.parentNode.childNodes[3].style.opacity = "0";
 
-    function display(disp, diff){
-        for (i = 0; i < a.length; i++){
-            if (a[i].childNodes[7].innerHTML.search(diff) + 1)
-            {
-                if (disp)
-                {
-                    a[i].parentNode.style.display="inline-block";
+    function display(disp, diff) {
+        for (i = 0; i < a.length; i++) {
+            if (a[i].childNodes[7].innerHTML.search(diff) + 1) {
+                if (disp) {
+                    a[i].parentNode.style.display = "inline-block";
                 }
-                else
-                {
-                    a[i].parentNode.style.display="none";
+                else {
+                    a[i].parentNode.style.display = "none";
                 }
             }
         }
@@ -143,7 +135,7 @@ function shuffle(array) {
 
 
 //Auto executes on page load
-document.addEventListener("DOMContentLoaded", function(){
+document.addEventListener("DOMContentLoaded", function () {
 
     sortProjects(document.getElementById("DATE"));
 
